e10b7697ace563c72c6fa6ad97512e30bfbefecc
Kalimye <1991818243@qq.com>
2018-08-05 16:06:14 +0800

    实现删除功能，添加新元素不能删除

--- js/index.js
+++ js/index.js
@@ -8,26 +8,39 @@
 
 var taskData = ["112", "544"];
 
+// 点击添加，弹出输入框
 function addTask() {
     document.getElementById("task-edit-box").style.display = "block";
+    document.querySelector(".task-title-detail").focus();
 }
+// 关闭输入框
 function closeWrite() {
     document.getElementById("task-edit-box").style.display = "none";
 }
+// 在ul中遍历数组元素
 var taskWrap = document.querySelector(".task-list");
-
-for (i = 0; i < taskData.length; i++) {
-    taskWrap.innerHTML += "<li>" + taskData[i] + "</li>";
+for (var i = 0; i < taskData.length; i++) {
+    taskWrap.innerHTML += "<li>" + taskData[i] + "<span class='delete-item'>删除</span></li>";
 }
+// 在页面中显示新添加的数组元素
 function submitCon() {
     var taskCon = document.querySelector(".task-title-detail").value;
     taskData.unshift(taskCon);
     document.querySelector(".task-title-detail").value = "";
     document.getElementById("task-edit-box").style.display = "none";
-    taskWrap.innerHTML += "<li>" + taskData[0] + "</li>";
+    taskWrap.innerHTML += "<li>" + taskData[0] + "<span class='delete-item'>删除</span></li>";
 }
 
-
+// 在页面中不显示 删除对应数组中的元素
+var taskItem = document.querySelectorAll(".task-list li");
+var deleteBtn = document.querySelectorAll(".delete-item");
+deleteBtn.forEach((item, index) => {
+    item.onclick = function () {
+        deleteBtn[index].style.display = "none";
+        taskItem[index].style.display = "none";
+        taskData.splice(index, 1);
+    }
+});
 // taskWrap.innerHTML = "<li>"+taskData[0].msg+"</li>";
 // 使用unshift
 

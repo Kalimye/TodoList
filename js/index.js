66d289b675f8a649f074a2143b23610ec5d63db6
Kalimye <1991818243@qq.com>
2018-08-06 14:17:39 +0800

    实现初步功能

--- js/index.js
+++ js/index.js
@@ -5,12 +5,13 @@
 //     msg: "写代码2",
 //     status: "待办2"
 // }];
-
-var taskData = ["112", "544"];
+var taskData = new Array();
+var wrap = document.querySelector('.wrap');
+var taskWrap = document.querySelector(".task-list");
 
 // 点击添加，弹出输入框
 function addTask() {
-    document.getElementById("task-edit-box").style.display = "block";
+    wrap.querySelector("#task-edit-box").style.display = "block";
     document.querySelector(".task-title-detail").focus();
 }
 // 关闭输入框
@@ -18,29 +19,61 @@ function closeWrite() {
     document.getElementById("task-edit-box").style.display = "none";
 }
 // 在ul中遍历数组元素
-var taskWrap = document.querySelector(".task-list");
-for (var i = 0; i < taskData.length; i++) {
-    taskWrap.innerHTML += "<li>" + taskData[i] + "<span class='delete-item'>删除</span></li>";
-}
+// for (var i = 0; i < taskData.length; i++) {
+// const li = document.createElement('li');
+// const span = document.createElement('span');
+// span.className = 'delete-item';
+// span.textContent = '删除';
+
+// li.appendChild(span);
+// console.log(li);
+//     taskWrap.innerHTML += "<li>" + taskData[i] + "<span class='delete-item'>删除</span></li>";
+//     taskWrap.innerHTML += `<li>${taskData[i]}<span class='delete-item'>删除</span><li>`;
+// }
+
+
 // 在页面中显示新添加的数组元素
+var inputVal = document.querySelector(".task-title-detail");
 function submitCon() {
-    var taskCon = document.querySelector(".task-title-detail").value;
-    taskData.unshift(taskCon);
-    document.querySelector(".task-title-detail").value = "";
-    document.getElementById("task-edit-box").style.display = "none";
-    taskWrap.innerHTML += "<li>" + taskData[0] + "<span class='delete-item'>删除</span></li>";
+    if (!inputVal.value) {
+        alert("不能为空");
+        closeWrite();
+    } else {
+        taskData.unshift(inputVal.value);
+        document.querySelector(".task-title-detail").value = "";
+        document.getElementById("task-edit-box").style.display = "none";
+        taskWrap.innerHTML += "<li>" + taskData[0] + "<span class='delete-item'>删除</span></li>";
+        taskWrap.setAttribute("isNull", "false");
+        itemDelete();
+    }
+}
+inputVal.onkeypress = function(){
+    if(event.keyCode == 13){
+        submitCon();
+    }
 }
 
 // 在页面中不显示 删除对应数组中的元素
-var taskItem = document.querySelectorAll(".task-list li");
-var deleteBtn = document.querySelectorAll(".delete-item");
-deleteBtn.forEach((item, index) => {
-    item.onclick = function () {
-        deleteBtn[index].style.display = "none";
-        taskItem[index].style.display = "none";
-        taskData.splice(index, 1);
-    }
-});
+function itemDelete() {
+    var taskItem = document.querySelectorAll(".task-list li");
+    var deleteBtn = document.querySelectorAll(".delete-item");
+    deleteBtn.forEach((item, index) => {
+        item.onclick = function () {
+            var r = confirm("确定要删除吗")
+            if (r == true) {
+                taskItem[index].remove();
+                taskData.splice(index, 1);
+                if (taskData[0] == undefined) {
+                    taskWrap.setAttribute("isNull", "true");
+                }
+            } else {
+                console.log("未删除")
+            }
+        }
+    });
+}
+itemDelete();
+
 
 // 加判断 如果ul里面有li显示 隐藏 “还没有添加过任务”
 // 页面中添加删除元素 是不是要用节点来控制呢？
